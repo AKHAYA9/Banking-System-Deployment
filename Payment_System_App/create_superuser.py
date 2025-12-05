d3885646d022481238ad0843ef3e32f604047b0a
AKHAYA9 <21cse273.akhayakumardash@giet.edu>
2025-12-05 16:32:37 +0530

    Create

---
+++ Payment_System_App/create_superuser.py
@@ -0,0 +1,29 @@
+# create_superuser.py
+import os
+import django
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "Payment_System.settings")
+django.setup()
+
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
+
+username = os.environ.get("DJANGO_SUPERUSER_USERNAME")
+email = os.environ.get("DJANGO_SUPERUSER_EMAIL")
+password = os.environ.get("DJANGO_SUPERUSER_PASSWORD")
+loginid = os.environ.get("DJANGO_SUPERUSER_LOGINID")  # Add this
+
+if username and email and password and loginid:
+    if not User.objects.filter(username=username).exists():
+        User.objects.create_superuser(
+            username=username,
+            email=email,
+            password=password,
+            loginid=loginid  # Pass the required field
+        )
+        print("Superuser created")
+    else:
+        print("Superuser already exists")
+else:
+    print("Missing environment variables for superuser")
